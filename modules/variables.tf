498cc3cbcf57f97ab47f06708676d95543cc33d5
Asura1201 <asurainferno1000@gmail.com>
2024-12-18 10:49:55 +0530

    Create variables.tf

---
+++ modules/variables.tf
@@ -0,0 +1,11 @@
+variable "rgname" {
+  description  = "Resource Group Name"
+  default      = "github-action1"
+  type         = string
+}
+
+variable "location" {
+  description  = "Azure Location"
+  default      = "East US"
+  type         = string
+}
