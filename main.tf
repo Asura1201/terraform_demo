51d543c7207fddc6ea2564b981c0d4c8735f7d25
Asura1201 <asurainferno1000@gmail.com>
2024-12-18 10:41:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform{
+    backend "azurerm"{
+      resource_group_name  = "TerraformDemo"
+      storage_account_name = "demoterranov2024"
+      container_name       = "tfstatefile"
+      key                  = "dev.terraform.tfstate"
+    }
+}
+module "RG" {
+  source   = "./modules/RG"
+  rgname   = var.rgname
+  location = var.location
+}
