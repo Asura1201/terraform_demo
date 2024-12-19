a3cc43a37850d1ef8ca4e6bfbdbfb66420326b15
Asura1201 <asurainferno1000@gmail.com>
2024-12-19 06:18:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,18 @@ variable "location" {
   default      = "East US"
   type         = string
 }
+
+variable "tfstate_rg" {
+   description  = "Rg for state file storage account"
+}
+
+variable "tfstate_storage_account_name" {
+   description  = "storage account to store tfstate file"
+}
+
+variable "tfstate_container_name" {
+   description  = "container to store tfstate file"
+}
+
+
+
